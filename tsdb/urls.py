f5e298d859cc8dae3f1cdafd00945beb6852bcff
Jim Zhang <hyriodula@gmail.com>
2015-01-01 01:14:50 -0500

    add gitignore, submission forms, remove __pycache__

--- tsdb/urls.py
+++ tsdb/urls.py
@@ -4,10 +4,20 @@ from tsdb import views
 urlpatterns = patterns('',
     # e.g. /
     url(r'^$', views.index, name='index'),
-    # e.g. /s/23
-    url(r'^s/(?P<song_id>\d+)$', views.song, name='song'),
+    # e.g. /song/23
+    url(r'^song/(?P<song_id>\d+)$', views.song, name='song'),
     # e.g. /t/69
     url(r'^t/(?P<translation_id>\d+)$', views.translation, name='translation'),
-    # e.g. /ts/69
-    url(r'^ts/(?P<translation_id>\d+)$', views.song_and_translation, name='song_and_translation'),
+    # e.g. /both/69
+    url(r'^both/(?P<translation_id>\d+)$', views.song_and_translation, name='song_and_translation'),
+    # i.e. /submit-song
+    url(r'^submit-song$', views.submit_song_form, name='submit_song_form'),
+    # i.e. /submit-song-action
+    url(r'^submit-song-action$', views.submit_song_action, name='submit_song_action'),
+    # i.e. /submit-both
+    url(r'^submit-both$', views.submit_song_and_translation_form, name='submit_song_and_translation_form'),
+    # e.g. /song/23/submit-translation
+    url(r'^song/(?P<song_id>\d+)/submit-translation$', views.submit_translation_form, name='submit_translation_form'),
+    # e.g. /song/23/submit-translation-action
+    url(r'^song/(?P<song_id>\d+)/submit-translation-action$', views.submit_translation_action, name='submit_translation_action'),
 )
--- tsdb/views.py
+++ tsdb/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from tsdb.models import Song, SongTranslation
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from tsdb.models import Artist, Song, SongTranslation
+import itertools
 
 def song(request, song_id):
     song = get_object_or_404(Song, pk=song_id)
@@ -25,12 +27,49 @@ def translation(request, translation_id):
 def song_and_translation(request, translation_id):
     translation = get_object_or_404(SongTranslation, pk=translation_id)
     song = translation.orig
-    lines = zip(song.lyrics.split('\n'), translation.lyrics.split('\n'))
+    lines = itertools.zip_longest(song.lyrics.split('\n'), translation.lyrics.split('\n'), fillvalue="")
     context = {'translation': translation, 'song': song, 'lines': lines}
     if 'youtube' in translation.video_url:
         yt_id = translation.video_url.split('=')[-1]
         context['song_video'] = '<iframe width="560" height="315" src="//www.youtube.com/embed/%s" frameborder="0" allowfullscreen></iframe>' % (yt_id)
     return render(request, 'tsdb/song_and_translation.html', context)
+
+def submit_song_action(request):
+    try:
+        new_song = Song()
+        new_song.title = request.POST['title']
+        new_song.video_url = request.POST['video_url']
+        new_song.artist, created = Artist.objects.get_or_create(name=request.POST['artist'])
+        new_song.lyrics = request.POST['lyrics']
+        new_song.save()
+        return HttpResponseRedirect(reverse('tsdb:song', args=(new_song.id,)))
+    except KeyError:
+        return HttpResponse("nope")
+
+def submit_song_form(request):
+    return render(request, 'tsdb/submit_song_form.html')
+
+def submit_translation_form(request, song_id):
+    song = get_object_or_404(Song, pk=song_id)
+    lines = song.lyrics.split('\n')
+    context = {'song': song, 'lines': lines}
+    return render(request, 'tsdb/submit_translation_form.html', context)
+
+def submit_translation_action(request, song_id):
+    try:
+        song = get_object_or_404(Song, pk=song_id)
+        new_trans = song.songtranslation_set.create()
+        new_trans.title = request.POST['title']
+        new_trans.video_url = request.POST['video_url']
+        new_trans.translator = request.POST['translator']
+        new_trans.lyrics = request.POST['lyrics']
+        new_trans.save()
+        return HttpResponseRedirect(reverse('tsdb:translation', args=(new_trans.id,)))
+    except KeyError:
+        return HttpResponse("nope")
+
+def submit_song_and_translation_form(request):
+    return render(request, 'tsdb/submit_song_and_translation_form.html')
 
 def index(request):
     songs = Song.objects.order_by('title')
